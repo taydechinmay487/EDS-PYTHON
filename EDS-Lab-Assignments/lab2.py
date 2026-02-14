db06b2aca3a523ffc6ddd5f1d4ad240b09ca0662
taydechinmay487 <taydechinmay487@gmail.com>
2026-02-14 11:40:25 +0530

    Create lab2.py

---
+++ EDS-Lab-Assignments/lab2.py
@@ -0,0 +1,10 @@
+# Arithmetic Operations
+a = 15
+b = 4
+
+print("\nArithmetic Operations")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
