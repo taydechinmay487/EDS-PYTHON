b419c896bcee45701a5813a395b3ceaaa97730ca
taydechinmay487 <taydechinmay487@gmail.com>
2026-02-14 11:39:33 +0530

    Create lab1.py

---
+++ EDS-Lab-Assignments/lab1.py
@@ -0,0 +1,18 @@
+print("----- Python Basics Lab -----")
+
+name = "Chinmay"
+age = 19
+height = 5.8
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student Status:", is_student)
+
+
+
+if age >= 18:
+    print("\nEligible for Voting")
+else:
+    print("\nNot Eligible")
